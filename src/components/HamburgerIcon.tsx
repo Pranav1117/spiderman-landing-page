6fbec2efb4be660d9f5bebada489ff3ca5c7f70d
pranav <pranavbavaskar1@gmail.com>
2025-03-21 22:39:24 +0530

    added navbar

---
+++ src/components/HamburgerIcon.tsx
@@ -0,0 +1,42 @@
+const HamburgerIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="30"
+      height="30"
+      viewBox="0,0,256,256"
+      className="cursor-pointer"
+    >
+      <g
+        fill="#ffffff"
+        fill-rule="nonzero"
+        stroke="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
+      >
+        <g transform="scale(6.4,6.4)">
+          <path d="M1.5,18.5h37v3h-37z"></path>
+          <path d="M38,19v2h-36v-2h36M39,18h-38v4h38v-4z"></path>
+          <path d="M1.5,8.5h37v3h-37z"></path>
+          <path d="M38,9v2h-36v-2h36M39,8h-38v4h38v-4z"></path>
+          <g>
+            <path d="M1.5,28.5h37v3h-37z"></path>
+            <path d="M38,29v2h-36v-2h36M39,28h-38v4h38v-4z"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default HamburgerIcon;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import HamburgerIcon from "./HamburgerIcon";
+import SearchIcon from "./SearchIcon";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-8">
+      <div className="flex gap-10 items-center">
+        <Image
+          src="/white-logo-spiderman.png"
+          height={80}
+          width={80}
+          alt="spiderman-logo"
+          className="cursor-pointer"
+        />
+        <HamburgerIcon />
+      </div>
+      <div className="flex gap-10 items-center">
+        <div className="flex items-center gap-3">
+          <Image src="/flag.png" height={30} width={30} alt="flag" />
+          <p>ENG</p>
+        </div>
+        <div>
+          <SearchIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
