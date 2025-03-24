33bd6d1347ec9d6e0e3d8df426eae52800598529
pranav <pranavbavaskar1@gmail.com>
2025-03-24 14:56:49 +0530

    added starting animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,82 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Autoplay,
-  Navigation,
-  Pagination,
-  EffectCoverflow,
-  Thumbs,
-} from "swiper/modules";
+import { Autoplay, Navigation, EffectCoverflow, Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Navbar from "@/components/Navbar";
-import { useEffect, useState } from "react";
 import gsap from "gsap";
 
 export default function Home() {
   const [slide, setSlide] = useState(null);
+  const handRef = useRef(null);
+  const startingAnimationEle = useRef(null);
+  const heroText = useRef(null);
+
   const items = [
-    { number: "01", src: "/spiderman-mask.png", bg: "#2C78BF" },
-    { number: "02", src: "/spiderman-hand.png", bg: "#8c311c" },
-    { number: "03", src: "/spiderman-helmet.png", bg: "#2C78BF" },
+    { number: "01", src: "/spiderman-hand.png", bg: "#7E3329" },
+    { number: "02", src: "/spiderman-mask.png", bg: "#1D5A90" },
+    { number: "03", src: "/spiderman-helmet.png", bg: "#242222" },
   ];
 
-  gsap.fromTo(
-    ".hand",
-    {
-      y: 1000,
-      ease:"elastic"
-    },
-    { y: 0 }
-  );
+  useEffect(() => {
+    gsap.fromTo(
+      handRef.current,
+      { y: 1000, opacity: 0 },
+      { y: 0, opacity: 1, ease: "sine.in", duration: 0.8 }
+    );
+
+    gsap.fromTo(
+      startingAnimationEle.current,
+      {
+        y: 100,
+        opacity: 0,
+        duration: 0.5,
+        delay: 1,
+        filter: "blur(5px)",
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        filter: "blur(0px)",
+      }
+    );
+    gsap.fromTo(
+      heroText.current,
+      {
+        y: 100,
+        opacity: 0,
+        duration: 0.5,
+        delay: 1,
+        filter: "blur(5px)",
+      },
+      {
+        opacity: 1,
+        delay: 1,
+        y: 0,
+        filter: "blur(0px)",
+      }
+    );
+  }, []);
+
   return (
     <div>
       {/* Navbar container */}
-      <div className="text-white">
+      <div className="text-white reveal-animation">
         <Navbar />
       </div>
 
       {/* Carousel */}
       <div className="flex">
         {/* HERO TEXT */}
-        <div className="absolute bottom-[25%] left-[17%] z-30 text-white">
+
+        <div
+          ref={heroText}
+          className="absolute bottom-[25%] left-[17%] z-30 text-white opacity-0"
+        >
           <h3 className="text-[4rem] font-bold">SPIDER</h3>
           <h3 className="text-[4rem] font-bold -mt-8">MAN</h3>
           <div className="mb-4 ">SUPERHERO</div>
@@ -63,14 +98,22 @@ export default function Home() {
           slidesPerView={1}
           loop={true}
           navigation={true}
-          pagination={{ clickable: true }}
-          modules={[Navigation, Pagination, Autoplay, EffectCoverflow, Thumbs]}
+          modules={[Navigation, Autoplay, EffectCoverflow, Thumbs]}
           className="w-full h-screen"
-          // @ts-ignore
           onSwiper={(swiper) => setSlide(swiper)}
           thumbs={{ swiper: slide }}
-          initialSlide={1}
+          speed={1000}
         >
+          <SwiperSlide className="flex justify-center items-center bg-[#CF5F45] h-screen w-full bg-custom ">
+            <Image
+              ref={handRef}
+              src="/spiderman-hand.png"
+              height={340}
+              width={340}
+              alt="item-preview"
+              className="absolute bottom-0 right-[31%] opacity-0 hand"
+            />
+          </SwiperSlide>{" "}
           <SwiperSlide className="flex justify-center items-center bg-[#2C78BF] h-screen w-full bg-custom">
             <Image
               src="/spiderman-mask.png"
@@ -80,16 +123,7 @@ export default function Home() {
               className="absolute -bottom-30 right-[23%]"
             />
           </SwiperSlide>
-          <SwiperSlide className="flex justify-center items-center bg-[#CF5F45] h-screen w-full bg-custom ">
-            <Image
-              src="/spiderman-hand.png"
-              height={340}
-              width={340}
-              alt="item-preview"
-              className="absolute bottom-0 right-[31%] hand"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="flex justify-center items-center bg-[#2C78BF] h-screen w-full bg-custom">
+          <SwiperSlide className="flex justify-center items-center bg-[#2d2d2d] h-screen w-full bg-custom">
             <Image
               src="/spiderman-helmet.png"
               height={500}
@@ -103,7 +137,8 @@ export default function Home() {
 
       <div className="absolute bottom-10 right-30">
         <Swiper
-          className="swiper thumbs w-[280px] h-[180px]"
+          ref={startingAnimationEle}
+          className="swiper thumbs w-[280px] h-[180px] opacity-0"
           spaceBetween={10}
           slidesPerView={2}
           onSwiper={setSlide}
@@ -113,9 +148,10 @@ export default function Home() {
           {items.map((item, index) => (
             <SwiperSlide
               key={index}
-              className={`flex gap-4 bg-[${item.bg}] bg-[##832e2c]`}
+              className={`flex gap-4 rounded cursor-pointer`}
+              style={{ background: item.bg }}
             >
-              <div className="number text-white text-xl relative -top-5 text-center z-100">
+              <div className="number text-white text-xl absolute left-1 top-0 text-center">
                 {item.number}
               </div>
               <Image
@@ -126,7 +162,6 @@ export default function Home() {
             </SwiperSlide>
           ))}
         </Swiper>
-        <hr className="mt-6" />
       </div>
     </div>
   );
